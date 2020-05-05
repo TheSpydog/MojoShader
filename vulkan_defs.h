5416dfdafdb89edbfb03e7db8f2028ab241c7d75
Evan Hemsley <evan@moonside.games>
2020-05-04 20:41:32 -0700

    fix defs

--- vulkan_defs.h
+++ vulkan_defs.h
@@ -1,210 +1,30 @@
-#if defined(__need_size_t)
-#if !defined(_SIZE_T) || __has_feature(modules)
-/* Always define size_t when modules are available. */
-#if !__has_feature(modules)
-#define _SIZE_T
-#endif
-typedef __SIZE_TYPE__ size_t;
-#endif
-#undef __need_size_t
-#endif /*defined(__need_size_t) */
+#include <stddef.h>
 
 /* per Vulkan 1.0 spec */
 
 /* In case this needs to be exported in a certain way... */
-#ifndef VKAPIENTRY
+#ifndef MOJOSHADER_VKAPIENTRY
 #ifdef _WIN32 /* Windows OpenGL uses stdcall */
-#define VKAPIENTRY __stdcall
+#define MOJOSHADER_VKAPIENTRY __stdcall
 #else
-#define VKAPIENTRY
+#define MOJOSHADER_VKAPIENTRY
 #endif
 #endif
 
-#ifndef VK_DEFINE_HANDLE
-#define VK_DEFINE_HANDLE(object) typedef struct object##_T* object;
+#ifndef MOJOSHADER_VK_DEFINE_HANDLE
+#define MOJOSHADER_VK_DEFINE_HANDLE(object) typedef struct MOJOSHADER_object##_T* object;
 #endif 
 
-#if !defined(VK_DEFINE_NON_DISPATCHABLE_HANDLE)
-#if defined(__LP64__) || defined(_WIN64) || (defined(__x86_64__) && !defined(__ILP32__) ) || defined(_M_X64) || defined(__ia64) || defined (_M_IA64) || defined(__aarch64__) || defined(__powerpc64__)
-		#define VK_DEFINE_NON_DISPATCHABLE_HANDLE(object) typedef struct object##_T *object;
-#else
-		#define VK_DEFINE_NON_DISPATCHABLE_HANDLE(object) typedef uint64_t object;
-#endif
+#ifndef MOJOSHADER_VKAPI_PTR 
+#define MOJOSHADER_VKAPI_PTR
 #endif
 
-#ifndef VKAPI_PTR 
-#define VKAPI_PTR
-#endif
+MOJOSHADER_VK_DEFINE_HANDLE(MOJOSHADER_VkDevice)
+MOJOSHADER_VK_DEFINE_HANDLE(MOJOSHADER_VkInstance)
 
-VK_DEFINE_HANDLE(VkDevice)
-VK_DEFINE_HANDLE(VkInstance)
+typedef void (MOJOSHADER_VKAPI_PTR *PFN_MOJOSHADER_vkVoidFunction)(void);
 
-VK_DEFINE_NON_DISPATCHABLE_HANDLE(VkBuffer)
-VK_DEFINE_NON_DISPATCHABLE_HANDLE(VkDeviceMemory)
-VK_DEFINE_NON_DISPATCHABLE_HANDLE(VkShaderModule)
-
-typedef unsigned long VkDeviceSize;
-typedef unsigned int VkFlags;
-typedef VkFlags VkBufferCreateFlags;
-typedef VkFlags VkBufferUsageFlags;
-typedef VkFlags VkMemoryMapFlags;
-typedef VkFlags VkShaderModuleCreateFlags;
-
-typedef enum VkResult {
-	VK_SUCCESS = 0,
-	VK_NOT_READY = 1,
-	VK_TIMEOUT = 2,
-	VK_EVENT_SET = 3,
-	VK_EVENT_RESET = 4,
-	VK_INCOMPLETE = 5,
-	VK_ERROR_OUT_OF_HOST_MEMORY = -1,
-	VK_ERROR_OUT_OF_DEVICE_MEMORY = -2,
-	VK_ERROR_INITIALIZATION_FAILED = -3,
-	VK_ERROR_DEVICE_LOST = -4,
-	VK_ERROR_MEMORY_MAP_FAILED = -5,
-	VK_ERROR_LAYER_NOT_PRESENT = -6,
-	VK_ERROR_EXTENSION_NOT_PRESENT = -7,
-	VK_ERROR_FEATURE_NOT_PRESENT = -8,
-	VK_ERROR_INCOMPATIBLE_DRIVER = -9,
-	VK_ERROR_TOO_MANY_OBJECTS = -10,
-	VK_ERROR_FORMAT_NOT_SUPPORTED = -11,
-	VK_ERROR_FRAGMENTED_POOL = -12,
-	VK_ERROR_UNKNOWN = -13,
-	VK_ERROR_OUT_OF_POOL_MEMORY = -1000069000,
-	VK_ERROR_INVALID_EXTERNAL_HANDLE = -1000072003,
-	VK_ERROR_FRAGMENTATION = -1000161000,
-	VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS = -1000257000,
-	VK_ERROR_SURFACE_LOST_KHR = -1000000000,
-	VK_ERROR_NATIVE_WINDOW_IN_USE_KHR = -1000000001,
-	VK_SUBOPTIMAL_KHR = 1000001003,
-	VK_ERROR_OUT_OF_DATE_KHR = -1000001004,
-	VK_ERROR_INCOMPATIBLE_DISPLAY_KHR = -1000003001,
-	VK_ERROR_VALIDATION_FAILED_EXT = -1000011001,
-	VK_ERROR_INVALID_SHADER_NV = -1000012000,
-	VK_ERROR_INCOMPATIBLE_VERSION_KHR = -1000150000,
-	VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT = -1000158000,
-	VK_ERROR_NOT_PERMITTED_EXT = -1000174001,
-	VK_ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT = -1000255000,
-	VK_THREAD_IDLE_KHR = 1000268000,
-	VK_THREAD_DONE_KHR = 1000268001,
-	VK_OPERATION_DEFERRED_KHR = 1000268002,
-	VK_OPERATION_NOT_DEFERRED_KHR = 1000268003,
-	VK_PIPELINE_COMPILE_REQUIRED_EXT = 1000297000,
-	VK_ERROR_OUT_OF_POOL_MEMORY_KHR = VK_ERROR_OUT_OF_POOL_MEMORY,
-	VK_ERROR_INVALID_EXTERNAL_HANDLE_KHR = VK_ERROR_INVALID_EXTERNAL_HANDLE,
-	VK_ERROR_FRAGMENTATION_EXT = VK_ERROR_FRAGMENTATION,
-	VK_ERROR_INVALID_DEVICE_ADDRESS_EXT = VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS,
-	VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS_KHR = VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS,
-	VK_ERROR_PIPELINE_COMPILE_REQUIRED_EXT = VK_PIPELINE_COMPILE_REQUIRED_EXT,
-	VK_RESULT_MAX_ENUM = 0x7FFFFFFF
-} VkResult;
-
-typedef enum VkStructureType {
-	VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO = 5,
-	VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO = 12,
-	VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO = 16
-} VkStructureType;
-
-typedef enum VkSharingMode {
-	VK_SHARING_MODE_EXCLUSIVE = 0
-} VkSharingMode;
-
-typedef enum VkBufferUsageFlagBits {
-	VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT = 0x00000010
-} VkBufferUsageFlagBits;
-
-typedef enum VkSystemAllocationScope {
-    VK_SYSTEM_ALLOCATION_SCOPE_COMMAND = 0,
-    VK_SYSTEM_ALLOCATION_SCOPE_OBJECT = 1,
-    VK_SYSTEM_ALLOCATION_SCOPE_CACHE = 2,
-    VK_SYSTEM_ALLOCATION_SCOPE_DEVICE = 3,
-    VK_SYSTEM_ALLOCATION_SCOPE_INSTANCE = 4,
-    VK_SYSTEM_ALLOCATION_SCOPE_MAX_ENUM = 0x7FFFFFFF
-} VkSystemAllocationScope;
-
-typedef enum VkInternalAllocationType {
-    VK_INTERNAL_ALLOCATION_TYPE_EXECUTABLE = 0,
-    VK_INTERNAL_ALLOCATION_TYPE_MAX_ENUM = 0x7FFFFFFF
-} VkInternalAllocationType;
-
-typedef void (VKAPI_PTR *PFN_vkVoidFunction)(void);
-
-typedef void* (VKAPI_PTR *PFN_vkAllocationFunction)(
-    void*                                       pUserData,
-    size_t                                      size,
-    size_t                                      alignment,
-    VkSystemAllocationScope                     allocationScope);
-
-typedef void* (VKAPI_PTR *PFN_vkReallocationFunction)(
-    void*                                       pUserData,
-    void*                                       pOriginal,
-    size_t                                      size,
-    size_t                                      alignment,
-    VkSystemAllocationScope                     allocationScope);
-
-typedef void (VKAPI_PTR *PFN_vkFreeFunction)(
-    void*                                       pUserData,
-    void*                                       pMemory);
-
-typedef void (VKAPI_PTR *PFN_vkInternalAllocationNotification)(
-    void*                                       pUserData,
-    size_t                                      size,
-    VkInternalAllocationType                    allocationType,
-    VkSystemAllocationScope                     allocationScope);
-
-typedef void (VKAPI_PTR *PFN_vkInternalFreeNotification)(
-    void*                                       pUserData,
-    size_t                                      size,
-    VkInternalAllocationType                    allocationType,
-    VkSystemAllocationScope                     allocationScope);
-
-typedef PFN_vkVoidFunction (VKAPI_PTR *PFN_vkGetDeviceProcAddr)(
-	VkDevice device,
+typedef PFN_MOJOSHADER_vkVoidFunction (MOJOSHADER_VKAPI_PTR *PFN_MOJOSHADER_vkGetDeviceProcAddr)(
+	MOJOSHADER_VkDevice device,
 	const char* pName
 );
-
-typedef struct VkAllocationCallbacks {
-    void*                                   pUserData;
-    PFN_vkAllocationFunction                pfnAllocation;
-    PFN_vkReallocationFunction              pfnReallocation;
-    PFN_vkFreeFunction                      pfnFree;
-    PFN_vkInternalAllocationNotification    pfnInternalAllocation;
-    PFN_vkInternalFreeNotification          pfnInternalFree;
-} VkAllocationCallbacks;
-
-typedef struct VkMemoryRequirements {
-	VkDeviceSize    size;
-	VkDeviceSize    alignment;
-	unsigned int    memoryTypeBits;
-} VkMemoryRequirements;
-
-typedef struct VkMemoryAllocateInfo {
-    VkStructureType    sType;
-    const void*        pNext;
-    VkDeviceSize       allocationSize;
-    unsigned int       memoryTypeIndex;
-} VkMemoryAllocateInfo;
-
-typedef struct VkShaderModuleCreateInfo {
-    VkStructureType              sType;
-    const void*                  pNext;
-    VkShaderModuleCreateFlags	 flags;
-    size_t                       codeSize;
-	const unsigned int*          pCode;
-} VkShaderModuleCreateInfo;
-
-typedef struct VkBufferCreateInfo {
-	VkStructureType        sType;
-	const void*            pNext;
-	VkBufferCreateFlags    flags;
-	VkDeviceSize           size;
-	VkBufferUsageFlags     usage;
-	int          		   sharingMode;
-	unsigned int           queueFamilyIndexCount;
-	const unsigned int*    pQueueFamilyIndices;
-} VkBufferCreateInfo;
-
-#define VULKAN_DEVICE_FUNCTION(ext, ret, func, params) \
-	typedef ret (VKAPIENTRY *vkfntype_##func) params;
-#include "mojoshader_vulkan_device_funcs.h"
-#undef VULKAN_DEVICE_FUNCTION
